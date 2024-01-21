3844bdb84f9bda1bbc06011242e105077647db3a
Lázaro Bodevan <lazarobodevan@gmail.com>
2024-01-21 18:35:23 -0300

    Fix: fixed scrolling catalog screen

--- lib/screens/nav_pages/catalog/catalog_screen.dart
+++ lib/screens/nav_pages/catalog/catalog_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/screens/nav_pages/catalog/components/products_tab.dart';
 import 'package:mobile/shared/components/custom_text_field.dart';
+import 'package:mobile/shared/components/product_tile.dart';
 import 'package:mobile/theme/theme_colors.dart';
 import 'package:mobile/theme/typography_styles.dart';
 
@@ -13,6 +14,8 @@ class CatalogScreen extends StatefulWidget{
 
 class _CatalogScreenState extends State<CatalogScreen> with TickerProviderStateMixin{
   late final TabController _tabController;
+  double appBarHeight = 150;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -30,36 +33,50 @@ class _CatalogScreenState extends State<CatalogScreen> with TickerProviderStateM
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(automaticallyImplyLeading: false),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomTextField(
-              controller: TextEditingController(),
-              prefixIcon: Icon(Icons.search),
-              hintText: "Buscar...",
-            ),
-            const SizedBox(height: 25,),
-            TabBar(
-              controller: _tabController,
-              indicatorColor: ThemeColors.primary3,
-              labelColor: ThemeColors.primary3,
-
-              tabs: [
-                Text("Produtos", style: TypographyStyles.paragraph3(),),
-                Text("Produtores", style: TypographyStyles.paragraph3(),),
-              ],
-            ),
-            const SizedBox(height: 20,),
-            Expanded(
-              child: TabBarView(controller: _tabController,children: [
-                ProductsTab(),
-                Center(child: Text("tchau"))
-              ]),
-            )
-          ],
+      body: SafeArea(
+        child: NestedScrollView(
+          physics: BouncingScrollPhysics(),
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                pinned: false,
+                floating: true,
+                automaticallyImplyLeading: false,
+                expandedHeight: appBarHeight,
+                elevation: 0,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 25),
+                          child: CustomTextField(controller: TextEditingController(), hintText: "Buscar...", prefixIcon: Icon(Icons.search),),
+                        ),
+                        TabBar(
+                          controller: _tabController,
+                          indicatorColor: ThemeColors.primary3,
+                          labelColor: ThemeColors.primary3,
+                          tabs: [
+                            Text("Produtos", style: TypographyStyles.paragraph3()),
+                            Text("Produtores", style: TypographyStyles.paragraph3()),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: TabBarView(
+            controller: _tabController,
+            children: [
+              Padding(padding:EdgeInsets.fromLTRB(20,10,20,0),child: ProductsTab()),
+              Container(child: Text("oi")),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/nav_pages/catalog/components/products_tab.dart
+++ lib/screens/nav_pages/catalog/components/products_tab.dart
@@ -6,15 +6,17 @@ class ProductsTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 2,
-      childAspectRatio: 0.8,
-      mainAxisSpacing: 10,
-      crossAxisSpacing: 10,
-      children: List.generate(
-        20,
-        (index) => ProductTile(),
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 10,
+        childAspectRatio: 0.8
       ),
+      itemBuilder: (context, index) {
+        return ProductTile();
+      },
+      itemCount: 50,
     );
   }
 }
